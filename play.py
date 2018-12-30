43c1ce888563b64dd86a1fac584e40a646c79182
makar-y <makarymalinouski@gmail.com>
2018-12-30 18:30:08 +0800

    Added visual change from dark to light

--- play.py
+++ play.py
@@ -11,12 +11,12 @@ from impro.Mind import Mind
 from vendor.UnitPlayer import UnitPlayer, CelloUnitPlayer, XyloUnitPlayer
 
 
-logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 logger = logging.getLogger()
 
 class App(object):
     note_color = {
-        "-" : ("white",),
+        "-" : ("#ffffff",),
         "C" : ("#FF0000", "#EF0000", "#DF0000", "#CF0000", "#BF0000", "#AF0000"), #red
         "Db": ("#FF6100", "#F85900", "#EF5100", "#E84800", "#DF4100", "#D83900"), #red-orange
         "D" : ("#FFA500", "#F79D00", "#EF9500"),#orange
@@ -26,10 +26,27 @@ class App(object):
         "Gb": ("#00FFFF", "#00DDDD", "#00BBBB", "#009999"), #green-blue
         "G" : ("#0000FF", "#0000DD", "#0000BB", "#0000AA", "#000099"), #blue
         "Ab": ("#000088", "#000077", "#000066",), #blue-darkblue
-        "A" : ("#000055", "#00004D", "#000045", "#00003D", "#000035"),
-        "Bb": ("#8000ff", "#8000ee", "#8000cc", "#8000aa", "#800099"),  #darkblue-purple
+        "A" : ("#000055", "#00004D", "#000045", "#00003D", "#000035"), #darkblue
+        "Bb": ("#550080", "#8000ee", "#8000cc", "#8000aa", "#800099"), #darkblue-purple
         "B" : ("#800080", "#780078", "#700070", "#680068", "#600060", "#500050") #purple
     }
+
+    note_color_dark = {
+        "-" : "ffffff",
+        "C" : "ff0000", #red
+        "Db": "ff4500", #red-orange
+        "D" : "ffa500", #orange
+        "Eb": "ffc500", #yellow-orange
+        "E" : "ffff00", #yellow
+        "F" : "9acd32", #yellow-green
+        "Gb": "00ff00", #green
+        "G" : "00ffff", #cyan
+        "Ab": "0000ff", #blue
+        "A" : "00008B", #darkblue
+        "Bb": "400080", #darkblue-purple
+        "B" : "800080"  #purple
+    }
+    
     keys = ["-", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
 
     WINDOW_WIDTH = 1411
@@ -44,13 +61,14 @@ class App(object):
                          height = App.WINDOW_HEIGHT)
    
         self.cs.pack()
+        self.key_dict = self.load_colors()
         logger.info("end app init")
         
 
     def play(self, instrument = "piano", key = None, mode = None,
              beat = (4,4), bpm = 60, max_mem = None,
              max_octave = 5, max_tone = 12, min_octave = 1, min_tone = 1):
-        print("play " + instrument)
+
         mind = Mind(instrument, key, mode, beat, bpm,
                     max_mem, max_octave, max_tone,
                     min_octave, min_tone)
@@ -69,7 +87,7 @@ class App(object):
             unit_player = UnitPlayer(bpm)
     
             mind.start_beat()
-
+        
         while not self.stop_flag:
             unit = mind.choose_unit()
             
@@ -77,13 +95,33 @@ class App(object):
                # self.random_rect(u.key)
                 logger.debug(u.key)
                 logger.debug(u.octave)
-                self.placed_rect(u.key, u.octave, u.duration,
-                                 mind.min_octave, mind.max_octave)
+                self.rect_dark_light(u.key, u.octave, u.duration,
+                                     mind.min_octave, mind.max_octave)
                 
                 self.master.update()
                 u.play(unit_player)
 
+                
+    def rect_dark_light(self, u_key, u_octave, u_dur, min_octave, max_octave):
+        seg_height = App.WINDOW_HEIGHT / (max_octave - min_octave + 1)
+        subseg_height = seg_height / 13
+        rand = random.randrange(App.WINDOW_WIDTH)
+        seg_width = App.WINDOW_WIDTH / (u_octave - min_octave + 1 if u_octave != 0 else 1)
+        subseg_width = seg_width / 13
+        if len(self.key_dict[u_key]) > 1:
+            color = self.key_dict[u_key].pop(0)
+        else:
+            color = self.key_dict[u_key][0]
+    
+        logger.debug("color {}".format(color))
+        
+        self.cs.create_rectangle(rand,
+                                 random.randrange(App.WINDOW_HEIGHT - seg_height * (u_octave - min_octave) - subseg_height * (App.keys.index(u_key) + 1), App.WINDOW_HEIGHT - seg_height * (u_octave - min_octave) - subseg_height * (App.keys.index(u_key))),
+                                 rand + App.WINDOW_WIDTH * Fraction(u_dur) / 2.0,
+                                 random.randrange(App.WINDOW_HEIGHT - seg_height * (u_octave - min_octave) - subseg_height * (App.keys.index(u_key) + 1), App.WINDOW_HEIGHT - seg_height * (u_octave - min_octave) - subseg_height * (App.keys.index(u_key))) + seg_width / (u_octave if u_octave !=0 else 1) / 3,
+                                 fill = color, outline = 'white', width = 2)
 
+        
     def placed_rect(self, u_key, u_octave, u_dur, min_octave, max_octave):
         seg_height = App.WINDOW_HEIGHT / (max_octave - min_octave + 1)
         subseg_height = seg_height / 13
@@ -107,6 +145,20 @@ class App(object):
         
     def stop(self):
         self.stop_flag = True
+
+
+    def load_colors(self):
+        key_dict = {}
+        for key in App.keys:
+            key_dict[key] = []
+            with open('vendor/resources/colors/{}.txt'.format(App.note_color_dark[key])) as f:
+                for line in f:
+                    if line.startswith('#'):
+                        key_dict[key].append(line.strip())
+                    
+        logger.debug(key_dict)
+        return key_dict
+    
     
 if __name__ == "__main__":
     argparser = ArgumentParser()
@@ -116,6 +168,9 @@ if __name__ == "__main__":
 
     app = App()
     app.play(args.instrument[0])
+
+
+    
 
         
         
